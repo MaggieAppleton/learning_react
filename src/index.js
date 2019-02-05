1f38d3d6cc66e0b3e6000b19fcd6f8ca4e3145ee
Maggie <maggie.fm.appleton@gmail.com>
2019-02-05 08:51:17 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from 'react-dom'
+import "./style.css"
+
+const App = () => {
+    return (
+        <div>
+            <h1>Does this still work?</h1>
+        </div>
+    )
+}
+
+render (<App />, document.getElementById("root"))
